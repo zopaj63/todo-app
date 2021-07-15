8521317ea303a68a4128a5e7af470c8165bde83a
zopaj63 <zoran.pajnic@gmail.com>
2021-07-15 19:42:53 +0200

    ponovno niz varijanta

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 class Task {
     constructor(title) {
         this.title = title;
+        this.isDone = false;
     }
 
     createElement() {
@@ -14,9 +15,19 @@ class Task {
             taskElement.remove();
         });
 
+        const checkboxElement = taskElement.querySelector("input[type='checkbox']");
+        checkboxElement.toggleAttribute("checked", this.isDone);
+        checkboxElement.addEventListener("change", (event) => {
+            event.preventDefault();
+            this.isDone = event.target.checked;
+        });
+
         return taskElement;
     }
 }
+
+const tasks = [];
+
 
 const taskForm = document.querySelector("[data-task-form]");
 const titleInput = taskForm.querySelector("[name=title]");
@@ -25,8 +36,13 @@ const taskList = document.querySelector("[data-task-list]");
 taskForm.addEventListener("submit", (event) => {
     event.preventDefault();
     const task = new Task(titleInput.value);
+    tasks.push(task);
+    taskList.innerHTML = "";
+    for (let i = 0; i < tasks.length; i++) {
+        taskList.appendChild(tasks[i].createElement());
+    }
 
-    taskList.appendChild(taskElement);
+    console.log(tasks);
 
 });
 
