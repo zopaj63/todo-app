2cdeb2d4d3ce19a957dcf66f0e44f4d8422a1f5d
zopaj63 <zoran.pajnic@gmail.com>
2021-07-15 19:00:06 +0200

    finish

--- app.js
+++ app.js
@@ -2,16 +2,31 @@ class Task {
     constructor(title) {
         this.title = title;
     }
-}
 
-const task = [];
+    createElement() {
+        const taskElement = document.createElement("div");
+        taskElement.innerHTML = `<label><input type="checkbox">${this.title}</label>`;
+
+        const removeButton = document.createElement('button');
+        removeButton.innerText = "X";
+        removeButton.addEventListener("click", (event) => {
+            event.preventDefault();
+            taskElement.remove();
+        });
+
+        return taskElement;
+    }
+}
 
 const taskForm = document.querySelector("[data-task-form]");
 const titleInput = taskForm.querySelector("[name=title]");
+const taskList = document.querySelector("[data-task-list]");
 
 taskForm.addEventListener("submit", (event) => {
     event.preventDefault();
-    task.push(new Task(titleInput.value));
-    console.log(task);
+    const task = new Task(titleInput.value);
+
+    taskList.appendChild(taskElement);
+
 });
 
