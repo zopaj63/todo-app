81951b536f27678fe1511a3728c5219217790ab1
zopaj63 <zoran.pajnic@gmail.com>
2021-10-06 17:01:19 +0200

    add scss

--- README.md
+++ README.md
@@ -5,8 +5,8 @@ Simple web application for creating and tracking todo tasks.
 ## Features
 
 - [x] Create a task
-- [ ] List all tasks
-- [ ] Mark a task as done
-- [ ] Delete a task
+- [x] List all tasks
+- [x] Mark a task as done
+- [x] Delete a task
 
 users, reminders, archiving, priorities, sorting, deadlines, animations...
--- app.js
+++ app.js
@@ -1,29 +1,29 @@
 class Task {
-    constructor(title) {
-        this.title = title;
-        this.isDone = false;
-    }
+  constructor(title) {
+    this.title = title;
+    this.isDone = false;
+  }
 
-    createElement() {
-        const taskElement = document.createElement("div");
-        taskElement.innerHTML = `<label><input type="checkbox">${this.title}</label>`;
+  createElement() {
+    const taskElement = document.createElement("div");
+    taskElement.innerHTML = `<label><input type="checkbox">${this.title}</label>`;
 
-        const removeButton = document.createElement('button');
-        removeButton.innerText = "X";
-        removeButton.addEventListener("click", (event) => {
-            event.preventDefault();
-            taskElement.remove();
-        });
+    const removeButton = document.createElement('button');
+    removeButton.innerText = "X";
+    removeButton.addEventListener("click", (event) => {
+      event.preventDefault();
+      taskElement.remove();
+    });
 
-        const checkboxElement = taskElement.querySelector("input[type='checkbox']");
-        checkboxElement.toggleAttribute("checked", this.isDone);
-        checkboxElement.addEventListener("change", (event) => {
-            event.preventDefault();
-            this.isDone = event.target.checked;
-        });
+    const checkboxElement = taskElement.querySelector("input[type='checkbox']");
+    checkboxElement.toggleAttribute("checked", this.isDone);
+    checkboxElement.addEventListener("change", (event) => {
+      event.preventDefault();
+      this.isDone = event.target.checked;
+    });
 
-        return taskElement;
-    }
+    return taskElement;
+  }
 }
 
 const tasks = [];
@@ -34,15 +34,79 @@ const titleInput = taskForm.querySelector("[name=title]");
 const taskList = document.querySelector("[data-task-list]");
 
 taskForm.addEventListener("submit", (event) => {
-    event.preventDefault();
-    const task = new Task(titleInput.value);
-    tasks.push(task);
-    taskList.innerHTML = "";
-    for (let i = 0; i < tasks.length; i++) {
-        taskList.appendChild(tasks[i].createElement());
-    }
+  //debugger;
+  event.preventDefault();
+  const task = new Task(titleInput.value);
+  tasks.push(task);
+  taskList.innerHTML = "";
+  for (let i = 0; i < tasks.length; i++) {
+    taskList.appendChild(tasks[i].createElement());
+  }
 
-    console.log(tasks);
+  console.log(tasks);
 
 });
 
+
+/*
+Domagojev kod:
+
+class Task {
+  constructor(title) {
+    this.title = title;
+    this.isDone = false;
+    this.isDeleted = false;
+  }
+
+  createElement() {
+    const taskElement = document.createElement("div");
+    taskElement.innerHTML = `<label><input type="checkbox" />${this.title}<button>X</button></label>`;
+
+    const removeButton = taskElement.querySelector("button");
+    removeButton.addEventListener("click", (event) => {
+      event.preventDefault();
+      this.isDeleted = true;
+      taskElement.remove();
+    });
+
+    const checkboxElement = taskElement.querySelector("input[type='checkbox']");
+    checkboxElement.toggleAttribute("checked", this.isDone);
+    checkboxElement.addEventListener("change", (event) => {
+      event.preventDefault();
+      this.isDone = event.target.checked;
+    });
+
+    return taskElement;
+  }
+}
+
+const taskForm = document.querySelector("[data-task-form]");
+const titleInput = taskForm.querySelector("[name=title]");
+
+class TaskList {
+  constructor() {
+    this.taskListElement = document.querySelector("[data-task-list]");
+    this.tasks = [];
+  }
+
+  refresh() {
+    this.taskListElement.innerHTML = "";
+    for (let i = 0; i < this.tasks.length; i++) {
+      const task = this.tasks[i];
+      if (!task.isDeleted) {
+        this.taskListElement.appendChild(task.createElement());
+      }
+    }
+  }
+}
+
+const taskList = new TaskList();
+
+taskForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const task = new Task(titleInput.value);
+
+  taskList.tasks.push(task);
+  taskList.refresh();
+});
+*/
